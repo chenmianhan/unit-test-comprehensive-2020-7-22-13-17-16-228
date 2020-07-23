47906650b11c36f89eb22ad40da897e81b1cc0de
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-23 19:00:17 +0800

    [GuessNumberGame][James Chen] feature: add  should_return_2A2B_when_answer_is_1234_when_given_1243 test

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -16,4 +16,15 @@ public class GuessNumberGameTest {
         //then
         assertEquals("4A0B", guessNumberGame.guess(guess));
     }
+
+    @Test
+    void should_return_2A2B_when_answer_is_1234_when_given_1243() {
+        String guess = "1234";
+
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+
+        //then
+        assertEquals("2A2B", guessNumberGame.guess(guess));
+    }
 }
