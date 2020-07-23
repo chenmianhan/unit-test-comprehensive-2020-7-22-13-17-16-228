c9b962df7c8b917df73e3426e6cc0b781a540599
CHENJA28 <CHENJA28@OOCL.COM>
2020-07-23 18:57:22 +0800

    [GuessNumberGame][James Chen] feature: add should_return_4A0B_when_answer_is_1234_when_given_1234 test

---
+++ src/main/java/example/GuessNumberGame.java
@@ -0,0 +1,8 @@
+package example;
+
+public class GuessNumberGame {
+
+    public String guess(String s) {
+        return null;
+    }
+}
---
+++ src/test/java/example/GuessNumberGameTest.java
@@ -0,0 +1,19 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GuessNumberGameTest {
+    @Test
+    void should_return_4A0B_when_answer_is_1234_when_given_1234() {
+        //given
+        String guess = "1234";
+
+        //when
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+
+        //then
+        assertEquals("4A0B", guessNumberGame.guess(guess));
+    }
+}
