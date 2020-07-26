bd445727b5849172b652bce062baa40ef248ce47
chenmianhan <mianhan1998@139.com>
2020-07-26 21:09:41 +0800

    [GuessNumber][James Chen] feature: implement should_return_false_when_digit_is_not_unique_given_1123 test

--- src/test/java/example/ValidatorTest.java
+++ src/test/java/example/ValidatorTest.java
@@ -18,4 +18,17 @@ class ValidatorTest {
         //then
         assertFalse(checkResult);
     }
+
+    @Test
+    void should_return_false_when_digit_is_not_unique_given() {
+        //given
+        String number = "1123";
+        Validator figureUniqueValidator = new FigureUniqueValidator();
+
+        //when
+        boolean checkResult = figureUniqueValidator.isValid(number);
+
+        //then
+        assertFalse(checkResult);
+    }
 }
