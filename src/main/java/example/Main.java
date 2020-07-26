63aa9e70cd09abf6df23aca650deb56d133d8f8d
chenmianhan <mianhan1998@139.com>
2020-07-26 20:38:00 +0800

    [GuessNumber][James Chen] feature: add main module

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -3,21 +3,22 @@ package example;
 import java.util.Scanner;
 
 public class Main {
-    
-
     public static void main(String[]args){
-        System.out.println("please input guess");
-        Scanner guessScanner=new Scanner(System.in);
         GuessNumberGame guessNumberGame=new GuessNumberGame(new RandomAnswerGenerator());
-        while(guessScanner.hasNext()){
+        System.out.println("Game Start!");
+        int maxTimes = 6, curTime = 0;
+        while (curTime < maxTimes) {
+            Scanner guessScanner = new Scanner(System.in);
             StringBuilder guess= new StringBuilder();
             for (int i = 0; i <4 ; i++) {
                 guess.append(guessScanner.nextInt());
             }
-
-            System.out.println(guessNumberGame.guess(guess.toString()));
+            String guessResult = guessNumberGame.guess(guess.toString());
+            System.out.println(guessResult);
+            curTime++;
+            if (guessResult.equals("4A0B")) break;
         }
-
+        if (curTime == maxTimes) System.out.println("Game Over!");
 
     }
 }
