6b6dede35f979c29b0af0cc62d4b541fb3b65af2
chenmianhan <mianhan1998@139.com>
2020-07-27 22:47:20 +0800

    [GuessNumber][James Chen] refactor: extract the constant VALIDATE_NUMBER_LENGTH

--- src/main/java/example/FigureUniqueValidator.java
+++ src/main/java/example/FigureUniqueValidator.java
@@ -4,14 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FigureUniqueValidator implements Validator {
+
+    private static final int VALIDATE_NUMBER_LENGTH = 4;
+
     @Override
     public boolean isValid(String number) {
-        if (number.length() != 4) return false;
+        if (number.length() != VALIDATE_NUMBER_LENGTH) return false;
         Set<Character> digitSet = new HashSet<>();
         for (Character digit : number.toCharArray()) {
             digitSet.add(digit);
         }
-        return digitSet.size() == 4;
+        return digitSet.size() == VALIDATE_NUMBER_LENGTH;
     }
 
 }
